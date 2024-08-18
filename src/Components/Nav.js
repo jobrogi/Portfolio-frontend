66cc47631da389ea01316a8d26b7322dac1098ea
Jon G <Jobrogi@gmail.com>
2024-08-18 04:51:38 -0400

    Mobile Friendly NavBar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState, useContext } from "react";
 import { RefContext } from "../App"; // Adjust the path as needed
 
 function Nav() {
+  var screenWidth = window.screen.width;
+  const [isMobile, setIsMobile] = useState(false);
+
   const { IntroRef, SkillsRef, ProjectsRef, PortfolioRef } =
     useContext(RefContext);
 
@@ -44,6 +47,16 @@ function Nav() {
     }
   }
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   //Event listener for listening for a mouse scroll wheel.
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -58,15 +71,23 @@ function Nav() {
   return (
     <div className="bg-ccDark w-full h-24 sticky top-0 font-Raleway font-black">
       {/* Nav ------------------------------------------------------------------------------------------------------------*/}
-      <div className="w-full h-24 bg-ccBlack px-20 flex flex-nowrap shadow-2xl shadow-black text-white fixed top-0 z-10">
-        <ul className="flex justify-start w-full h-24 items-center ">
-          <li className="ms-2 me-2 text-2xl">
-            <button>Contact</button>
-          </li>
-          <li className="ms-2 me-2 text-2xl">
-            <button>Socials</button>
-          </li>
-        </ul>
+      <div className="w-full h-24 bg-ccBlack lg:px-18 flex flex-nowrap shadow-2xl shadow-black text-white fixed top-0 z-10 md:px-6 px-4">
+        {isMobile === false ? (
+          <ul className="flex justify-start w-full h-24 items-center ">
+            <button>
+              <i class="fa-solid fa-bars text-2xl"></i>
+            </button>
+          </ul>
+        ) : (
+          <ul className="flex justify-start w-full h-24 items-center ">
+            <li className="ms-2 me-2 text-2xl">
+              <button>Contact</button>
+            </li>
+            <li className="ms-2 me-2 text-2xl">
+              <button>Socials</button>
+            </li>
+          </ul>
+        )}
 
         <li
           className={`flex items-center justify-center h-24 left-0 w-full pointer-events-none transition-all duration-1000   ${
@@ -82,52 +103,58 @@ function Nav() {
             }}
           >
             {" "}
-            Jonathon Gilliam
+            {isMobile === false ? "Jonathon Gilliam" : "J Gilliam"}
           </button>
         </li>
 
-        <ul className="flex justify-end w-full h-24 items-center">
-          <li
-            className={`ms-2 me-2 text-2xl transition-all ${
-              liActive === 1 ? "text-3xl text-orange-500" : ""
-            }`}
-          >
-            <button
-              className=""
-              onClick={() => {
-                SkillsRef.current.scrollIntoView({ behavior: "smooth" });
-              }}
+        {isMobile === true ? (
+          <ul className="flex justify-end w-full h-24 items-center">
+            <i class="fa-solid fa-bars text-2xl"></i>
+          </ul>
+        ) : (
+          <ul className="flex justify-end w-full h-24 items-center">
+            <li
+              className={`ms-2 me-2 text-2xl transition-all ${
+                liActive === 1 ? "text-3xl text-orange-500" : ""
+              }`}
             >
-              Skills
-            </button>
-          </li>
-          <li
-            className={`ms-2 me-2 text-2xl transition-all ${
-              liActive === 2 ? "text-3xl text-orange-500" : ""
-            }`}
-          >
-            <button
-              onClick={() => {
-                ProjectsRef.current.scrollIntoView({ behavior: "smooth" });
-              }}
+              <button
+                className=""
+                onClick={() => {
+                  SkillsRef.current.scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Skills
+              </button>
+            </li>
+            <li
+              className={`ms-2 me-2 text-2xl transition-all ${
+                liActive === 2 ? "text-3xl text-orange-500" : ""
+              }`}
             >
-              Projects
-            </button>
-          </li>
-          <li
-            className={`ms-2 me-2 text-2xl transition-all ${
-              liActive === 3 ? "text-3xl text-orange-500" : ""
-            }`}
-          >
-            <button
-              onClick={() => {
-                PortfolioRef.current.scrollIntoView({ behavior: "smooth" });
-              }}
+              <button
+                onClick={() => {
+                  ProjectsRef.current.scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Projects
+              </button>
+            </li>
+            <li
+              className={`ms-2 me-2 text-2xl transition-all ${
+                liActive === 3 ? "text-3xl text-orange-500" : ""
+              }`}
             >
-              Portfolio
-            </button>
-          </li>
-        </ul>
+              <button
+                onClick={() => {
+                  PortfolioRef.current.scrollIntoView({ behavior: "smooth" });
+                }}
+              >
+                Portfolio
+              </button>
+            </li>
+          </ul>
+        )}
       </div>
       {/* -----------------------------------------------------------------------------------------------------------------*/}
     </div>
